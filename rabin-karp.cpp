9ee1b271c3e9482a9c5fa357d02e89f74af166cc
Aadish Deshpande <32305224+add19@users.noreply.github.com>
2018-10-05 17:44:53 +0530

    Updated rabin-karp

--- rabin-karp.cpp
+++ rabin-karp.cpp
@@ -1,67 +1,76 @@
-#include<bits/stdc++.h>
-using namespace std;
+#infndef DSALGLIB_RABINKARP_H
+#define DSALGLIB_RABINKARP_H
 
+#include "array.h"
+namespace dsa{
 
-int prime = 101;
+	static const long long int mod = 1e9 + 7;
+	long long int prime = 101;
 
-long reCalculateHash(string text, int oldIndex,int newIndex, long oldHash, int pattern_length){
-	long newHash = oldHash - text[oldIndex];
-	newHash	/= prime;
-	newHash = newHash + text[newIndex] * pow(prime, pattern_length - 1);
-	return newHash;
-}	
-
-long createHash(string str, int end){
-	long hashVal = 0;
-	for(int i=0; i<end; i++){
-		hashVal += str[i] * pow(prime,	i);
-	}
-	return hashVal;
-}
-
-bool checkString(string pattern, string text_snip, int pat_start, int pat_end, int text_start, int text_end){
-	while(pat_start < pat_end && text_start < text_end){
-		if(pattern[pat_start] != text_snip[text_start]){
-			return false;
+	template<typename type>
+// modified recursive power function
+	long long pow(long long x, long long y){
+		if(y == 0){
+			return 1;
+		}if(y == 1){
+			return x%mod;
+		}
+		else if(y % 2 == 1){
+			return ((x%mod) * pow((x%mod * x%mod)%mod, y/2))%mod; 
 		}else{
-			pat_start++;
-			text_start++;
-		}
+			return pow(((x%mod) * (x%mod))%mod, y/2)%mod;
+		}		
+	}
+
+	template<typename type>
+// rolling hash
+	long long int reCalculateHash(array<type> text,long long int oldIndex,long long int newIndex, long long int oldHash, long long int pattern_length){
+		long long int newHash = (oldHash - text[oldIndex])%mod;
+		newHash	/= prime;
+		newHash = (newHash + text[newIndex] * pow(prime, pattern_length - 1))%mod;
+		return newHash;
 	}	
-	return true;
-}
 
-int patternSearch(string text, string pattern){
-	int m = pattern.size();
-	int n = text.size();
-
-	long patternHash = createHash(pattern, m);
-	long textHash 	 = createHash(text, m);
-	for(int i=0; i<n - m + 1; i++){
-		if(patternHash == textHash && checkString(pattern, text, 0, m, i, i + m)){
-			return i;
-		}else if(i < n - m + 1){
-			textHash = reCalculateHash(text, i, i + m, textHash, m);
+	template<typename type>
+// creates a hash value
+	long long int createHash(array<type> str, long long int end){
+		long long int hashVal = 0;
+		for(long long int i=0; i<end; i++){
+			hashVal = (hashVal + (str[i] * pow(prime,	i))%mod)%mod; 
 		}
-	}
-	return -1;
-}
-
-int main(){
-
-	string text;
-	string pattern;
-	cout << "Enter the text: ";
-	cin >> text;
-	cout << "Enter the pattern to search in the text: ";
-	cin >> pattern;
-	
-	int index = patternSearch(text, pattern);
-	if(index != -1){
-		cout << "Shift to " << index + 1 << " places to get the pattern in text" << endl;
-	}else{
-		cout << "Pattern not found" << endl;
+		return hashVal;
 	}
 
-	return 0;
+	template<typename type>
+// checks if two strings of same hash value are equal or not
+	bool checkString(array<type> pattern, array<type> text_snip, long long int pat_start, long long int pat_end, long long int text_start, long long int text_end){
+		while(pat_start < pat_end && text_start < text_end){
+			if(pattern[pat_start] != text_snip[text_start]){
+				return false;
+			}else{
+				pat_start++;
+				text_start++;
+			}
+		}	
+		return true;
+	}
+
+	template<typename type>
+// finds the valid/invalid shift for pattern in text
+	int patternSearch(array<type> text, array<type> pattern){
+		long long int m = pattern.size();
+		long long int n = text.size();
+
+		long long int patternHash = createHash(pattern, m);
+		long long int textHash 	 = createHash(text, m);
+		for(long long int i=0; i<n - m + 1; i++){
+			if(patternHash == textHash && checkString(pattern, text, 0, m, i, i + m)){
+				return i;
+			}else if(i < n - m + 1){
+				textHash = reCalculateHash(text, i, i + m, textHash, m);
+			}
+		}
+		return -1;
+	}
 }
+#endif	//DSALGLIB_RABINKARP_H
